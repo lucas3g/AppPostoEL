3ae4c105afc0c58668f8599ba43a8fec9910a923
lucassilva080397 <lucassilva080397@gmail.com>
2021-10-10 18:23:08 -0300

    no message

--- app_posto_el/lib/src/pages/login/controller/login_settings.dart
+++
@@ -1,9 +0,0 @@
-import 'package:shared_preferences/shared_preferences.dart';
-
-class LoginSettings {
-  Future<SharedPreferences> _preferences = SharedPreferences.getInstance();
-
-  Future<void> getInstance() async {
-    SharedPreferences preferences = await _preferences;
-  }
-}
