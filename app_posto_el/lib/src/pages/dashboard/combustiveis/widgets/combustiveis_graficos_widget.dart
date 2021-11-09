348e29b1d15f11a2ca8f24203b65ea044531ca0a
lucassilva080397 <lucassilva080397@gmail.com>
2021-11-09 14:09:02 -0300

    ajustes nas libs

--- app_posto_el/lib/src/pages/dashboard/combustiveis/widgets/combustiveis_graficos_widget.dart
+++ app_posto_el/lib/src/pages/dashboard/combustiveis/widgets/combustiveis_graficos_widget.dart
@@ -24,26 +24,6 @@ class CombustiveisGraficoWidget extends StatelessWidget {
     Color(0xFFb7ba00),
   ];
 
-  String retornaDescricao({required String desc}) {
-    String descricao;
-    if (desc.contains('GASOLINA C COMUM')) {
-      descricao = 'GC';
-    } else if (desc.contains('GASOLINA ADITIVADA')) {
-      descricao = 'GA';
-    } else if (desc.contains('DIESEL B S500 PETROBRAS GRID')) {
-      descricao = 'DB S500 GRID';
-    } else if (desc.contains('DIESEL B S500 COMUM')) {
-      descricao = 'DB S500 C';
-    } else if (desc.contains('OLEO DIESEL B S10')) {
-      descricao = 'OD B S10';
-    } else if (desc.contains('ETANOL COMUM')) {
-      descricao = 'EC';
-    } else {
-      descricao = 'DESCRICAO NAO ENCONTRADA';
-    }
-    return descricao;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Column(
